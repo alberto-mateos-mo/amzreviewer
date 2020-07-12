bb3acdc06be2660de78d4ff9cc01577c3b7a1102
David Mateos <cldav.privmath@gmail.com>
2020-07-12 00:04:29 -0500

    Wordcloud plots & limit to 50

--- R/mod_reviews.R
+++ R/mod_reviews.R
@@ -23,7 +23,7 @@ mod_reviews_ui <- function(id){
           tags$p("Paste Amazon Product Link"),
           shinymaterial::material_text_box(input_id = ns("code"), "", icon = "link"),
           tags$p("Enter number of pages to scrape"),
-          shinymaterial::material_number_box(input_id = ns("pages"), "", min_value = 1, max_value = 10, initial_value = 5),
+          shinymaterial::material_number_box(input_id = ns("pages"), "", min_value = 1, max_value = 50, initial_value = 10),
           actionButton(ns("go"), "Analyse!")
         )
       ),
@@ -31,8 +31,8 @@ mod_reviews_ui <- function(id){
         width = 9,
         shinymaterial::material_card(
           shinyglide::glide(
-            shinyglide::screen(plotOutput(ns("adjs"))),
-            shinyglide::screen(plotOutput(ns("keyw")))
+            shinyglide::screen(label = "Adjectives mentioned in reviews:", plotOutput(ns("adjs"))),
+            shinyglide::screen(label = "Keywords found in reviews:", plotOutput(ns("keyw")))
           )
         )
       )
@@ -61,25 +61,17 @@ mod_reviews_server <- function(input, output, session){
     adj_stats <- udpipe::txt_freq(adj_stats$lemma)
     shinymaterial::material_spinner_hide(session, "adjs")
     
-    ggplot2::ggplot(head(adj_stats, 20), ggplot2::aes(reorder(key, freq_pct), freq_pct))+
-      ggplot2::geom_point(colour = "#223150")+
-      ggplot2::geom_linerange(ggplot2::aes(ymin = 0, ymax = freq_pct), colour = "#223150")+
-      ggplot2::labs(x = "", y = "Frequency of ocurrence (%)")+
-      ggplot2::ggtitle("Adjectives Most Mentioned in Reviews")+
-      ggplot2::coord_flip()+
-      ggplot2::theme_minimal()
+    ggplot2::ggplot(adj_stats, ggplot2::aes(label = key, size = freq_pct), colour = "#020200")+
+      ggwordcloud::geom_text_wordcloud(shape = "square")+
+      ggplot2::scale_size(range = c(1, 15))
   })
   
   output$keyw <- renderPlot({
     rake_stats <- udpipe::keywords_rake(x(), term = "lemma", group = "doc_id", relevant = x()$upos %in% c("NOUN", "ADJ"))
     
-    ggplot2::ggplot(head(rake_stats, 20), ggplot2::aes(reorder(keyword, rake), rake))+
-      ggplot2::geom_point(colour = "#223150")+
-      ggplot2::geom_linerange(ggplot2::aes(ymin = 0, ymax = rake), colour = "#223150")+
-      ggplot2::labs(x = "", y = "RAKE Score")+
-      ggplot2::ggtitle("Keywords Found in Reviews")+
-      ggplot2::coord_flip()+
-      ggplot2::theme_minimal()
+    ggplot2::ggplot(rake_stats, ggplot2::aes(label = keyword, size = rake*freq))+
+      ggwordcloud::geom_text_wordcloud(shape = "square")+
+      ggplot2::scale_size(range = c(1, 15))
   })
   
 }
