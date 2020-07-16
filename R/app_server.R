7a4d5f63934531a79c9e9af910051edd8a001474
David Mateos <cldav.privmath@gmail.com>
2020-07-15 22:28:18 -0500

    stops when session ends

--- R/app_server.R
+++ R/app_server.R
@@ -5,5 +5,13 @@
 #' @import shiny
 #' @noRd
 app_server <- function( input, output, session ) {
+  
+  if (!interactive()) {
+    session$onSessionEnded(function() {
+      stopApp()
+      q("no")
+    })
+  }
+  
   callModule(mod_reviews_server, "reviews_ui_1")
 }
